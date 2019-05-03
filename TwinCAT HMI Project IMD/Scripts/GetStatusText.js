c4c88699f9249c9e25d46aff30f5806913b81d10
Pim-dijk <31619916+Pim-dijk@users.noreply.github.com>
2019-05-03 16:04:40 +0200

    Added StatusText Usercontrol. Fiddled around with different ADS Configs for publishing to a IPC

---
+++ TwinCAT HMI Project IMD/Scripts/GetStatusText.js
@@ -0,0 +1,27 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var GetStatusText = function (Symbol,itemID,StatusTextList) {
+        //console.log(Symbol);
+        //console.log(itemID);
+        //console.log(StatusTextList);
+
+        var locale = TcHmi.Locale.get();
+        //console.log(locale);
+
+        //console.log(StatusTextList[locale][itemID][Symbol]);
+
+        var text = StatusTextList[locale][itemID][Symbol];
+        return text;
+    };
+    
+    TcHmi.Functions.registerFunction('GetStatusText', GetStatusText);
+})(TcHmi);
